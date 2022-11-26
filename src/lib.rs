7bfad4f1221010f42e27bee38f9efac71449a6d8
Kuya Kohara <kuya.kohara@gmail.com>
2022-11-26 10:18:23 +0900

    fix: clear by black

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -22,8 +23,16 @@ pub fn main(id: &str) {
 
     match document.get_element_by_id(id) {
         Some(el) => {
-            let canvas = document.create_element("canvas").expect("createElement failed.");
-            el.append_child(&canvas).expect("appendChild failed.");
+            let o = document.create_element("canvas").expect("createElement failed.");
+            el.append_child(&o).expect("appendChild failed.");
+            let canvas = o.dyn_into::<web_sys::HtmlCanvasElement>().expect("failed to cast into HtmlCanvasElement");
+            let context = canvas.get_context("webgl2")
+                .expect("getContext('webgl2') failed.")
+                .expect("getContext('webgl2') retuned null.")
+                .dyn_into::<web_sys::WebGl2RenderingContext>().
+                expect("failed to cast into WebGl2RenderingContext");
+            context.clear_color(0.0, 0.0, 0.0, 1.0);
+            context.clear(web_sys::WebGl2RenderingContext::COLOR_BUFFER_BIT);
         },
         None => {
         }
